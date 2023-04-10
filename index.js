ad1910cb43501baba2613804b618d26e03535639
Chandler Farley <chandlerfarley@live.com>
2023-04-10 15:52:14 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record){
+    const SBWin = record.find(obj => obj.result === "W");
+    if (!!SBWin === true) {
+        return SBWin.year
+    } else {
+        return undefined;
+    }
+}
